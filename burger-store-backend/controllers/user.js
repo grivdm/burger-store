87aca22731d747e36cf5d14cc07f9f873694cece
grivdm <grivdm94@gmail.com>
2023-02-21 23:33:11 -0500

    refactor models and routes + valid id handle

--- burger-store-backend/controllers/user.js
+++ burger-store-backend/controllers/user.js
@@ -2,20 +2,34 @@ const User = require("../models/user");
 const createError = require("http-errors");
 require("express-async-errors");
 
+// check if user is exist by id
+// this middleware is used in the following routes:
+// GET a single user by ID
+// PUT an existing user by ID
+// DELETE an existing user by ID
+// PUT an existing user password by ID
+exports.checkIfUserExist = async (req, res, next) => {
+  const userInDb = await User.findById(req.params.id);
+  if (!userInDb) {
+    return next(createError.NotFound("User not found"));
+  }
+  req.userInDb = userInDb;
+  next();
+};
+
+
+// GET all users
 exports.getAllUsers = async (req, res, next) => {
   const users = await User.find();
   res.status(200).json(users);
 };
 
+// GET a single user by ID
 exports.getUserById = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  if (user) {
-    res.status(200).json(user);
-  } else {
-    return next(createError(400, "User not found"));
-  }
+  res.status(200).json(req.userInDb);
 };
 
+// POST a new user
 exports.createUser = async (req, res, next) => {
   try {
     const existingUser = await User.findOne({ email: req.body.email });
@@ -27,27 +41,48 @@ exports.createUser = async (req, res, next) => {
     await user.save();
     res.status(201).json(user);
   } catch (error) {
-    return next(createError(400, error.message))
+    return next(createError(400, error.message));
   }
 };
 
+
+// PUT an existing user by ID
 exports.updateUserById = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    try {
-      const updatedUser = await user.save();
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      return next(createError(400, err.message));
-    }
-  } else {
-    return next(createError.NotFound("User not found"));
+  const user = req.userInDb;
+  user.name = req.body.name || user.name;
+  user.email = req.body.email || user.email;
+  try{
+    const updatedUser = await user.save();
+    res.status(200).json(updatedUser);
+  } catch (err){
+    return next(createError(400, err.message));
   }
 };
 
+// PUT an existing user password by ID
+exports.updateUserPasswordById = async (req, res, next) => {
+  const user = req.userInDb;
+  user.password = req.body.password || user.password;
+  try{
+    const updatedUser = await user.save();
+    res.status(200).json(updatedUser);
+  } catch (err){
+    return next(createError(400, err.message));
+  }
+
+}
+
+// GET orders of a user by ID
+exports.getOrdersOfUserById = async (req, res, next) => {
+  const user = req.userInDb;
+  const orders = await user.populate("orders").execPopulate();
+  res.status(200).json(orders);
+};
+
+
+// DELETE an existing user by ID
 exports.deleteUserById = async (req, res, next) => {
-  await User.findByIdAndDelete(req.params.id);
+  const userId = req.userInDb._id;
+  await User.findByIdAndDelete(userId);
   res.status(204).send();
 };
